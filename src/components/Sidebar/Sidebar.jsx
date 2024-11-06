b7380010bd0037e9fcd62c1447a373a197b60e29
Aparna Pradeep <aparnapradeep31@gmail.com>
2024-11-06 16:11:06 +0530

    top and bottom added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import './Sidebar.css'
+import {assets} from '../../assets/assets'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
+        <div className="sidebar_top">
+          <div className="Menu">  
+            <img src=  {assets.menu_icon} alt="" />  
+          </div>
+          <div className="Newchat">
+            <img src=  {assets.plus_icon} alt="" />
+            <p>New Chat</p>
+          </div>
+          <div className="recent">
+            <p className="recent_title">Recent</p>
+            <div className="recent_chats">
+              <img src=  {assets.message_icon} alt="" />
+              <p>What is react..</p>
+            </div>
+          </div>
+
+          
+
+        </div>
+        <div className="sidebar_bottom"> 
+          <div className="bottom-parts">
+            <img src= {assets.question_icon} alt="" />
+            <p>Help</p>
+           
+
+          </div>
+          <div className="bottom-parts">
+            <img src= {assets.history_icon} alt="" />
+            <p>History</p>
+           
+
+          </div>
+          <div className="bottom-parts">
+            <img src= {assets.setting_icon} alt="" />
+            <p>Settings</p>
+           
+
+          </div>
+      
+
         
+        </div>   
+
       
     </div>
   )
