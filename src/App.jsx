03efb6712384a750e9749f71fb809d6117c3a350
Aparna Pradeep <aparnapradeep31@gmail.com>
2024-11-14 14:00:15 +0530

    mainbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
+import Mainbar from './components/Mainbar/Mainbar'
 
 const App = () => {
   return (
     <>
-    <Sidebar /> 
+    <Sidebar />
+    <Mainbar/>
       
     </>
   )
---
+++ src/components/Mainbar/Mainbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Mainbar.css'
+import { assets } from '../../assets/assets'
+
+const Mainbar = () => {
+  return (
+    <div className="main">
+        <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+        </div>
+        <div className = "main_container">
+            <div className="greet">
+                <p><span>Hello, Aparna.</span></p>
+                <p>How can I help you today?</p>
+
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest places to visit</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Explain about Blockchain</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>AI Model Training Dataset</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>IEEE Citation Formating</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>  
+            </div>
+
+        </div>
+    </div>
+    
+  
+  )
+}
+
+export default Mainbar
